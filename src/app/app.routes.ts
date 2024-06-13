e634386f3c03ccd98646b5e411b7261c5db6b354
Chris A. Thillet <blizero.entertainment@gmail.com>
2024-06-13 00:04:56 -0600

    Path not found route added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import {DashboardComponent} from "./components/dashboard/dashboard.component";
+import {PathNotFoundComponent} from "./components/path-not-found/path-not-found.component";
 
 export const routes: Routes = [
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   {path: 'dashboard', component: DashboardComponent},
+  { path: '**', redirectTo: '404' },
+  { path: '404', component: PathNotFoundComponent },
   // {path: 'events', component: DashboardComponent},
   // {path: 'notifications', component: DashboardComponent},
 ];
