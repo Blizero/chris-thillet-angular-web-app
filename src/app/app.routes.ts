ff6cbe5badfb522c58f79beccf697060c3ceca27
Chris A. Thillet <blizero.entertainment@gmail.com>
2024-06-12 21:06:37 -0600

    Angular routes improved

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
-import {LayoutContainerComponent} from "./components/layout-container/layout-container.component";
 import {DashboardComponent} from "./components/dashboard/dashboard.component";
 
 export const routes: Routes = [
-  {path: '**', redirectTo: 'dashboard'},
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'events', component: DashboardComponent},
   {path: 'notifications', component: DashboardComponent},
