0d73b04fdd8e453d9121a7692883d26b2badc912
Chris A. Thillet <blizero.entertainment@gmail.com>
2024-06-21 15:47:08 -0600

    Improve CORS

--- app.py
+++ app.py
@@ -6,10 +6,9 @@ from flask_cors import CORS, cross_origin
 from pymongo import MongoClient, collection
 from bson.objectid import ObjectId
 
-
 app = create_app()
 
 # The under if __name__ code block
-port = os.environ.get("PORT", 5000) # Heroku will set the PORT environment variable
+port = os.environ.get("PORT", 5000)  # Heroku will set the PORT environment variable
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=port)  #running your server on development mode, setting debug to True
--- config.py
+++ config.py
@@ -4,5 +4,5 @@ import os
 class Config:
     DEBUG = True
     GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
-    MONGO_URI_GOOGLE_AI = 'mongodb://localhost:27017/google_ai_api'
+    # MONGO_URI_GOOGLE_AI = 'mongodb://localhost:27017/google_ai_api' # Enable for local development
     MONGO_URI_GOOGLE_CLOUD = os.getenv('MONGO_URI_GOOGLE_CLOUD')
